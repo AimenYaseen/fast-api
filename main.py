ddfd7362cc31c26b73e478b2ff1b109270512669
AimenYaseen <aimenyaseen6@gmail.com>
2024-12-17 16:58:51 +0500

    predefined values in path parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
@@ -12,9 +13,10 @@ def health_check():
     return {"detail": "Working Great!"}
 
 
+# -------------- PATH PARAMETERS ---------------------
 @app.get("/items/{item_id}")
 def read_item(
-        item_id: int,
+        item_id: int,  # path parameter with its type
         q: Union[str, None] = None  # optional argument
 ):
     return {
@@ -23,6 +25,25 @@ def read_item(
     }
 
 
+# ------------------- PREDEFINED VALUES ------------------
+class ModelName(str, Enum):
+    test = "test"
+    staging = "staging"
+    live = "live"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    models = {
+        ModelName.test: {"model_name": ModelName.test, "message": "implement and test locally!"},
+        ModelName.staging: {"model_name": ModelName.staging, "message": "deployed on a server and test it"},
+        ModelName.live: {"model_name": ModelName.live, "message": "deployed to production server and make it available"
+                                                                  "to users"},
+    }
+    return models.get(model_name)
+
+
+# -------------------- PYDANTIC SCHEMAS -------------------------
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {
