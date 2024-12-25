f0537b42b7140582ddf7e1849f70ec8f0b20e4c4
AimenYaseen <aimenyaseen6@gmail.com>
2024-12-25 11:56:05 +0500

    added validations to body using pydantic Field

--- schemas.py
+++ schemas.py
@@ -5,9 +5,16 @@ from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
-    description: str | None = None  # optional argument using new syntax
-    price: float
-    tax: float = 0
+    description: str | None = Field(
+        default=None,
+        title="The description of the item",
+        max_length=300
+    )
+    price: float = Field(
+        gt=0,
+        description="The price must be greater than zero"
+    )
+    tax: float | None = None  # optional argument using new syntax
     is_offered: bool = True  # made it optional with default value as True
 
 
@@ -15,4 +22,4 @@ class FilterParams(BaseModel):
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list[str] = []
+    tags: list[str] = None
