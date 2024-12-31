3f3a9b76b5a8445ea62dad3e7853f9031bdfacac
AimenYaseen <aimenyaseen6@gmail.com>
2024-12-31 17:10:00 +0500

    pydantic models as headers

--- 6_header_param.py
+++ 6_header_param.py
@@ -1,16 +1,29 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class CommonHeaders(BaseModel):
+    model_config = {
+        "extra": "forbid",
+    }
+
+    host: str
+    save_data: bool
+    x_tag: list[str] = []
+
+
 @app.get("/items/")
 async def read_items(
+        headers: Annotated[CommonHeaders, Header()],  # pydantic model as headers
         user_agent: Annotated[str | None, Header()] = None,
-        x_token: Annotated[list[str] | None, Header()] = None, # receive duplicate headers as a list
+        x_token: Annotated[list[str] | None, Header()] = None,  # receive duplicate headers as a list
 ):
     return {
         "User-Agent": user_agent,
         "X-Token": x_token,
+        "Common_Headers": headers,
     }
