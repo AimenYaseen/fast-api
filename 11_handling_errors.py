bb8816e23c221c99ba4ed884491ea010676908aa
AimenYaseen <aimenyaseen6@gmail.com>
2025-01-22 15:08:55 +0500

    exception handling

--- 11_handling_errors.py
+++ 11_handling_errors.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, HTTPException
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from starlette.requests import Request, HTTPException as StarletteHTTPException
+from starlette.responses import JSONResponse, PlainTextResponse
 
 app = FastAPI()
 
-items = {"foo": "The Foo Wrestlers"}
+items = {
+    "foo": "The Foo Wrestlers"
+}
 
 
 @app.get("/items-header/{item_id}")
@@ -18,19 +22,19 @@ async def read_item_header(item_id: str):
     return {"item": items[item_id]}
 
 
-# Custom Exception Handler
+# ----------------------------- CUSTOM EXCEPTION HANDLER ----------------------------------
 class UnicornException(Exception):
     def __init__(self, name: str):
         self.name = name
 
 
-@app.exception_handlers(UnicornException)
+@app.exception_handler(UnicornException)
 async def unicorn_exception_handler(request: Request, exc: UnicornException):
     return JSONResponse(
         status_code=418,
         content={
             "message": f"Oops! {exc.name} did something. There goes a rainbow...",
-        },
+        }
     )
 
 
@@ -39,3 +43,32 @@ async def read_unicorn(name: str):
     if name == "yolo":
         raise UnicornException(name=name)
     return {"unicorn_name": name}
+
+
+# ---------------------------- OVERRIDE DEFAULT EXCEPTION HANDLER ------------------------
+
+# It affects requests after as well as before this exception handler
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    # return PlainTextResponse(str(exc), status_code=400)
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(
+            {
+                "detail": exc.errors(),
+                "body": exc.body,
+            }
+        )
+    )
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
