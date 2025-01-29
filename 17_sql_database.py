b80ed9f602c825815a51e1e459965c2af293f1e2
AimenYaseen <aimenyaseen6@gmail.com>
2025-01-29 18:34:18 +0500

    separate out pydantic public and private schemas

--- 17_sql_database.py
+++ 17_sql_database.py
@@ -12,11 +12,28 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
 # Hero sql model just like pydantic
-class Hero(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     age: int | None = Field(default=None, index=True)
+
+
+class Hero(HeroBase, table=True):
+    id: int = Field(primary_key=True)
     secret_name: str
+
+
+class HeroPublic(HeroBase):
+    id: int
+
+
+class HeroCreate(HeroBase):
+    secret_name: str
+
+
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
 
 
 def create_db_and_tables():
@@ -38,25 +55,38 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDepends) -> Hero:
-    session.add(hero)
+@app.post("/heroes/", response_model=HeroPublic)
+def create_hero(hero: HeroCreate, session: SessionDepends):
+    db_hero = Hero.model_validate(hero)
+    session.add(db_hero)
     session.commit()
-    session.refresh(hero)
-    return hero
+    session.refresh(db_hero)
+    return db_hero
+
+@app.patch("/heroes/{hero_id}", response_model=HeroPublic)
+def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDepends):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
 
 
-@app.get("/heroes/")
+@app.get("/heroes/", response_model=list[HeroPublic])
 def read_heroes(
         session: SessionDepends,
         offset: int = 0,
         limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
+):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
 
 
-@app.get("/heroes/{hero_id}")
+@app.get("/heroes/{hero_id}", response_model=HeroPublic)
 def read_hero(hero_id: int, session: SessionDepends):
     hero = session.get(Hero, hero_id)
     if not hero:
