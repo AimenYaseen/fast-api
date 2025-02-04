520881c8c796a4097c2da6fc996fa7ff14ccf39a
AimenYaseen <aimenyaseen6@gmail.com>
2025-02-04 11:58:58 +0500

    environment variables and settings

---
+++ advance_guide/env_variables.py
@@ -0,0 +1,30 @@
+from functools import lru_cache
+
+from fastapi import Depends, FastAPI
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import Annotated
+
+
+class Settings(BaseSettings):
+    app_name: str = "VARIABLE APP"
+    admin_email: str = ""
+    items_per_user: int = 50
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+app = FastAPI()
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+@app.get("/info")
+async def info(settings: Annotated[Settings, Depends(get_settings)]):
+    return {
+        "app_name": settings.app_name,
+        "admin_email": settings.admin_email,
+        "items_per_user": settings.items_per_user,
+    }
