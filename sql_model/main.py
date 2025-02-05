6093c4714a89fb1e5763ac3b80398d6149d13450
AimenYaseen <aimenyaseen6@gmail.com>
2025-02-05 16:39:44 +0500

    create hero objects

--- sql_model/main.py
+++ sql_model/main.py
@@ -1,5 +1,7 @@
+from sqlalchemy.orm import Session
 from sqlmodel import create_engine, SQLModel
-from . import models
+
+from .models import Hero
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -7,8 +9,36 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 
+def create_heroes():
+    hero_1 = Hero(name="Deadpond2", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy2", secret_name="Pedro Parqeador")
+    hero_3 = Hero(name="Rusty-Man2", secret_name="Tommy Sharp", age=48)
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+
+        session.commit()
+
+        session.refresh(hero_1)
+        session.refresh(hero_2)
+        session.refresh(hero_3)
+
+        print("After refreshing the heroes")
+        print("Hero 1:", hero_1)
+        print("Hero 2:", hero_2)
+        print("Hero 3:", hero_3)
+    # print heroes with ids
+    print("After the session closes")
+    print("Hero 1:", hero_1)
+    print("Hero 2:", hero_2)
+    print("Hero 3:", hero_3)
+
+
 def create_db_and_models():
     SQLModel.metadata.create_all(engine)
+    create_heroes()
 
 
 if __name__ == "__main__":
