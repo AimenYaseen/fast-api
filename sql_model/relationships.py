6ed33475359d4ff00fc30846ee09eb92e7e06152
AimenYaseen <aimenyaseen6@gmail.com>
2025-02-06 17:44:20 +0500

    cascade_delete and on_delete attributes

--- sql_model/relationships.py
+++ sql_model/relationships.py
@@ -1,3 +1,7 @@
+"""
+Allow foreign keys in SQLITE Database
+PRAGMA foreign_keys = ON;
+"""
 from sqlmodel import SQLModel, Field, create_engine, Session, Relationship, select
 
 
@@ -6,7 +10,8 @@ class Team(SQLModel, table=True):
     name: str = Field(index=True)
     headquarters: str
 
-    heroes: list["Hero"] = Relationship(back_populates="team")
+    # cascade should be set in another model
+    heroes: list["Hero"] = Relationship(back_populates="team", cascade_delete=True)
 
 
 class Hero(SQLModel, table=True):
@@ -15,7 +20,8 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: int | None = Field(default=None)
 
-    team_id: int | None = Field(default=None, foreign_key="team.id")
+    # on delete is set in the foreign key, other options => SET NULL, RESTRICT
+    team_id: int | None = Field(default=None, foreign_key="team.id", ondelete="CASCADE")
     team: Team | None = Relationship(back_populates="heroes")
 
 
@@ -61,37 +67,37 @@ def create_heroes():
         session.refresh(hero_spider_boy)
         print("Updated hero:", hero_spider_boy)
 
-    # ----------------- Create New Team with Multiple Heroes -----------------------
-    hero_black_lion = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
-    hero_sure_e = Hero(name="Princess Sure-E", secret_name="Sure-E")
-    team_wakaland = Team(
-        name="Wakaland",
-        headquarters="Wakaland Capital City",
-        heroes=[hero_black_lion, hero_sure_e],
-    )
-    session.add(team_wakaland)
-    session.commit()
-    session.refresh(team_wakaland)
-    print("Team Wakaland:", team_wakaland)
+        # ----------------- Create New Team with Multiple Heroes -----------------------
+        hero_black_lion = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+        hero_sure_e = Hero(name="Princess Sure-E", secret_name="Sure-E")
+        team_wakaland = Team(
+            name="Wakaland",
+            headquarters="Wakaland Capital City",
+            heroes=[hero_black_lion, hero_sure_e],
+        )
+        session.add(team_wakaland)
+        session.commit()
+        session.refresh(team_wakaland)
+        print("Team Wakaland:", team_wakaland)
 
-    # ----------------- ADD MORE HEROES TO TEAM -------------------------------
-    hero_tarantula = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
-    hero_dr_weird = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
-    hero_cap = Hero(
-        name="Captain North America", secret_name="Esteban Rogelios", age=93
-    )
+        # ----------------- ADD MORE HEROES TO TEAM -------------------------------
+        hero_tarantula = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+        hero_dr_weird = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+        hero_cap = Hero(
+            name="Captain North America", secret_name="Esteban Rogelios", age=93
+        )
 
-    team_preventers.heroes.append(hero_tarantula)
-    team_preventers.heroes.append(hero_dr_weird)
-    team_preventers.heroes.append(hero_cap)
-    session.add(team_preventers)
-    session.commit()
-    session.refresh(hero_tarantula)
-    session.refresh(hero_dr_weird)
-    session.refresh(hero_cap)
-    print("Preventers new hero:", hero_tarantula)
-    print("Preventers new hero:", hero_dr_weird)
-    print("Preventers new hero:", hero_cap)
+        team_preventers.heroes.append(hero_tarantula)
+        team_preventers.heroes.append(hero_dr_weird)
+        team_preventers.heroes.append(hero_cap)
+        session.add(team_preventers)
+        session.commit()
+        session.refresh(hero_tarantula)
+        session.refresh(hero_dr_weird)
+        session.refresh(hero_cap)
+        print("Preventers new hero:", hero_tarantula)
+        print("Preventers new hero:", hero_dr_weird)
+        print("Preventers new hero:", hero_cap)
 
 
 def select_heroes():
@@ -110,10 +116,46 @@ def select_heroes():
 
         print("Preventers heroes:", team_preventers.heroes)
 
+
+def update_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        result = session.exec(statement)
+        hero_spider_boy = result.one()
+
+        hero_spider_boy.team = None
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_spider_boy)
+        print("Spider-Boy without team:", hero_spider_boy)
+
+def delete_team():
+    with Session(engine) as session:
+        statement = select(Team).where(Team.name == "Wakaland")
+        team = session.exec(statement).one()
+        session.delete(team)
+        session.commit()
+        print("Deleted team:", team)
+
+        # --------------- CHECK HERO OBJECT DELETED OR NOT
+        statement = select(Hero).where(Hero.name == "Black Lion")
+        result = session.exec(statement)
+        hero = result.first()
+        print("Black Lion not found:", hero)
+
+        statement = select(Hero).where(Hero.name == "Princess Sure-E")
+        result = session.exec(statement)
+        hero = result.first()
+        print("Princess Sure-E not found:", hero)
+
+
 def main():
     # create_db_and_tables()
     # create_heroes()
-    select_heroes()
+    # select_heroes()
+    # update_heroes()  # update relationships
+    delete_team()
 
 
 if __name__ == "__main__":
