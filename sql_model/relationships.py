a0afd617ad88f199b9ad83b46ea93b5def616759
AimenYaseen <aimenyaseen6@gmail.com>
2025-02-06 16:15:45 +0500

    create and update heroes and teams

--- sql_model/relationships.py
+++ sql_model/relationships.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, create_engine, Session, select, Relationship
+from sqlmodel import SQLModel, Field, create_engine, Session, Relationship
 
 
 class Team(SQLModel, table=True):
@@ -28,8 +28,75 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
+def create_heroes():
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sisters Margaret's Bar")
+        hero_deadpond = Hero(
+            name="Dead-Pond", secret_name="Dive Wilson", team=team_z_force,
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man", secret_name="Tommy Sharp", age=48, team=team_preventers,
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Paequeador")
+
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
+
+        # ------------------------- UPDATE RELATIONSHIP ------------------
+        hero_spider_boy.team = team_preventers
+        session.add(hero_spider_boy)
+        session.commit()
+        session.refresh(hero_spider_boy)
+        print("Updated hero:", hero_spider_boy)
+
+    # ----------------- Create New Team with Multiple Heroes -----------------------
+    hero_black_lion = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_sure_e = Hero(name="Princess Sure-E", secret_name="Sure-E")
+    team_wakaland = Team(
+        name="Wakaland",
+        headquarters="Wakaland Capital City",
+        heroes=[hero_black_lion, hero_sure_e],
+    )
+    session.add(team_wakaland)
+    session.commit()
+    session.refresh(team_wakaland)
+    print("Team Wakaland:", team_wakaland)
+
+    # ----------------- ADD MORE HEROES TO TEAM -------------------------------
+    hero_tarantula = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_dr_weird = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_cap = Hero(
+        name="Captain North America", secret_name="Esteban Rogelios", age=93
+    )
+
+    team_preventers.heroes.append(hero_tarantula)
+    team_preventers.heroes.append(hero_dr_weird)
+    team_preventers.heroes.append(hero_cap)
+    session.add(team_preventers)
+    session.commit()
+    session.refresh(hero_tarantula)
+    session.refresh(hero_dr_weird)
+    session.refresh(hero_cap)
+    print("Preventers new hero:", hero_tarantula)
+    print("Preventers new hero:", hero_dr_weird)
+    print("Preventers new hero:", hero_cap)
+
+
 def main():
-    create_db_and_tables()
+    # create_db_and_tables()
+    create_heroes()
 
 
 if __name__ == "__main__":
