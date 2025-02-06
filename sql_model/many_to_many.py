376695450c7e6d8a14848da2806a41f94a86e001
AimenYaseen <aimenyaseen6@gmail.com>
2025-02-06 18:20:18 +0500

    update and remove objects in many to many relationships

--- sql_model/many_to_many.py
+++ sql_model/many_to_many.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine, Field, Relationship, Session
+from sqlmodel import SQLModel, create_engine, Field, Relationship, Session, select
 
 
 class HeroTeamLink(SQLModel, table=True):
@@ -67,6 +67,28 @@ def create_heroes():
         print("Rusty-Man Teams:", hero_rusty_man.teams)
         print("Spider-Boy:", hero_spider_boy)
         print("Spider-Boy Teams:", hero_spider_boy.teams)
+
+
+def update_heroes():
+    with Session(engine) as session:
+        hero_spider_boy = session.exec(
+            select(Hero).where(Hero.name == "Spider-Boy")
+        ).one()
+        team_z_force = session.exec(select(Team).where(Team.name == "Z-Force")).one()
+
+        team_z_force.heroes.append(hero_spider_boy)
+        session.add(team_z_force)
+        session.commit()
+
+        print("Updated Spider-Boy's Teams:", hero_spider_boy.teams)
+        print("Z-Force heroes:", team_z_force.heroes)
+
+        hero_spider_boy.teams.remove(team_z_force)
+        session.add(team_z_force)
+        session.commit()
+
+        print("Reverted Z-Force's heroes:", team_z_force.heroes)
+        print("Reverted Spider-Boy's teams:", hero_spider_boy.teams)
 
 
 def main():
